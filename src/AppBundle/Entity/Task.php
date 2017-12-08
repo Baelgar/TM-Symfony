0c182317200cc6d7c0cc387989e1dcaf28152c7e
Baelgar <enfoux.kevin@gmail.com>
2017-12-08 10:48:13 +0100

    Task Manager Symfony

---
+++ src/AppBundle/Entity/Task.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Task
+ *
+ * @ORM\Table(name="task")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TaskRepository")
+
+ */
+class Task
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\ManyToOne(targetEntity="TaskList", inversedBy="tasks")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="due", type="datetime")
+     */
+    private $due;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="completed", type="boolean")
+     */
+    private $completed;
+
+    private $taskList;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Task
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Task
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set due
+     *
+     * @param \DateTime $due
+     *
+     * @return Task
+     */
+    public function setDue($due)
+    {
+        $this->due = $due;
+
+        return $this;
+    }
+
+    /**
+     * Get due
+     *
+     * @return \DateTime
+     */
+    public function getDue()
+    {
+        return $this->due;
+    }
+
+    /**
+     * Set completed
+     *
+     * @param boolean $completed
+     *
+     * @return Task
+     */
+    public function setCompleted($completed)
+    {
+        $this->completed = $completed;
+
+        return $this;
+    }
+
+    /**
+     * Get completed
+     *
+     * @return bool
+     */
+    public function getCompleted()
+    {
+        return $this->completed;
+    }
+}
